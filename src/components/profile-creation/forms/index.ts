9aa6e845205572f15df3519d835fa5f64adef059
Kshitijknk07 <knk200407@gmail.com>
2025-09-17 09:32:50 +0530

    fixing

--- src/components/profile-creation/forms/index.ts
+++
@@ -1,7 +0,0 @@
-export { FarmerForm } from './FarmerForm';
-export { WildCollectorForm } from './WildCollectorForm';
-export { ProcessorForm } from './ProcessorForm';
-export { LaboratoryForm } from './LaboratoryForm';
-export { ManufacturerForm } from './ManufacturerForm';
-export { PackerForm } from './PackerForm';
-export { StorageForm } from './StorageForm';
--- src/components/profile-creation/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProfileCreation } from './ProfileCreation';
-export * from './types';
-export * from './hooks/useProfileForms';
-export * from './forms';
